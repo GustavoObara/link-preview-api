580385f79a4956dcf92e59c668db36cb15c96d8c
GustavoObara <luizobara@gmail.com>
2024-10-08 12:39:53 -0300

    Alterando comentários

--- app.py
+++ app.py
@@ -7,42 +7,42 @@ import re
 app = Flask(__name__)
 CORS(app)
 
-# Função para remover "R$", espaços e converter para float
 def clean_price(price_text):
-    """ Remove qualquer símbolo, mantém apenas números e a vírgula, depois formata o número no padrão brasileiro. """
-    # Remove todos os caracteres não numéricos ou vírgula
-    numeric_price = re.sub(r'[^\d,]', '', price_text)
-    
-    # Converte para float usando a vírgula como separador decimal
-    float_price = float(numeric_price.replace(',', '.'))
-    
-    # Formata o número no padrão brasileiro com 2 casas decimais, ponto como separador de milhar e vírgula como decimal
-    return "{:,.2f}".format(float_price).replace(",", "v").replace(".", ",").replace("v", ".")
+    """ 
+    Limpa a string de preço e transforma em um formato bonito para exibição.
+    """
+    numeric_price = re.sub(r'[^\d,]', '', price_text)  # Remove tudo que não é dígito ou vírgula
+    float_price = float(numeric_price.replace(',', '.'))  # Converte pra float
+    return "{:,.2f}".format(float_price).replace(",", "v").replace(".", ",").replace("v", ".")  # Formata o preço
 
 def parse_price(price_text):
-    """ Converte o preço formatado como string no estilo brasileiro para float. """
-    numeric_price = re.sub(r'[^\d,]', '', price_text)
-    return float(numeric_price.replace(',', '.'))
+    """ 
+    Transforma a string de preço para float, para realizar comparações. Em casos de produtos
+    vendidos com o preço maior do que o produto cadastrado.
+    """
+    numeric_price = re.sub(r'[^\d,]', '', price_text)  # Remove os lixos
+    return float(numeric_price.replace(',', '.'))  # Retorna o preço em float
 
 def scrape_page(url):
+    """ 
+    Realiza uma busca nos elementos da pagina e retorna um objeto referente a pagina
+    do produto oferecido. Com título, preço, imagem, site e a própria url.
+    """
     headers = {'User-Agent': 'Mozilla/5.0'} 
     response = requests.get(url, headers=headers)
     
-    if response.status_code != 200:
+    if response.status_code != 200:  #Se não conseguir pegar a página, retorna None
         return None
     
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Captura do título e imagem do produto
-    title = soup.find('meta', property='og:title') or soup.find('title')
-    image = soup.find('meta', property='og:image') or soup.find('img', {'id': 'landingImage'})
+    title = soup.find('meta', property='og:title') or soup.find('title')  #Tenta pegar o título
+    image = soup.find('meta', property='og:image') or soup.find('img', {'id': 'landingImage'})  #Tenta pegar a imagem
     
-    price = None
+    price = None # Inicializa a variável de preço
 
-    # Scraping para Amazon
     if "amazon" in url:
         try:
-            # Captura do preço
             price_whole = soup.find('span', {'class': 'a-price-whole'})
             price_fraction = soup.find('span', {'class': 'a-price-fraction'})
             if price_whole and price_fraction:
@@ -50,106 +50,85 @@ def scrape_page(url):
             elif price_whole:  
                 price = clean_price(price_whole.text.strip())
             else:
-                price = 'No price available'
+                price = 'No price available' 
         except Exception as e:
-            print("Something went wrong:", e)
-            price = 'Error retrieving price'
+            print("Algo deu errado:", e)
+            price = 'Error retrieving price'  
 
-    # Scraping para Kabum
     if "kabum" in url:
         try:
-            price_pc = None
-            # Se o URL contém "monte-seu-pc", busca pelo ID "totals"
-            if "monte-seu-pc" in url:
-                price_pc = soup.find("h4", id="totals")
-            else:
-                # Captura do preço final para produtos regulares
-                price_element = soup.find("h4", class_="finalPrice")
-
-            # Processamento do preço
-            if price_pc:
-                # Se 'price_pc' for encontrado, usa ele
-                price = clean_price(price_pc.get_text(strip=True))
-            elif price_element:
-                # Caso contrário, usa o 'finalPrice' (para outros produtos)
+            price_element = soup.find("h4", class_="finalPrice")
+            if price_element:
                 price = clean_price(price_element.get_text(strip=True))
             else:
-                # Se nenhum preço for encontrado
-                price = 'No price available'
-                
+                price = 'No price available'                
         except Exception as e:
-            print("Something went wrong:", e)
+            print("Algo deu errado:", e)
             price = 'Error retrieving price'
 
-    # Scraping para Steam
     if "steam" in url:
         try:
-            # Obtém o preço completo e o preço com desconto
             discount = soup.find("div", class_="discount_final_price")
             price_element = soup.find("div", class_="price")
 
-            fullPrice = None
-            discountPrice = None
+            fullPrice, discountPrice = None, None  
 
             if price_element:
-                fullPrice = clean_price(price_element.get_text(strip=True))
+                fullPrice = clean_price(price_element.get_text(strip=True))  # Pega o preço normal
 
             if discount:
-                discountPrice = clean_price(discount.get_text(strip=True))
+                discountPrice = clean_price(discount.get_text(strip=True))  # Pega o preço com desconto
 
-            # print(f"Full Price: {fullPrice}, Discount Price: {discountPrice}")
-
-            # Verifica qual preço é menor
             if discountPrice is not None:
                 if parse_price(fullPrice) < parse_price(discountPrice):
-                    price = fullPrice
+                    price = fullPrice  # Se o preço normal for menor, fica com ele
                 else:
-                    price = discountPrice
+                    price = discountPrice  # Se não, fica com o desconto
             else:
-                price = fullPrice
-            
-            # print(f"Final Price: {price}")
+                price = fullPrice  # Se não tem desconto, fica com o normal
 
             if price is None:
-                price = 'Erro ao buscar jogo +18'
-                
-
+                price = 'Erro ao buscar jogo +18' # Jogos +18 não são abertos na pagina do jogo e sim em um formulário para verificação da idade, vale pesquisar como passar por esse formulário para acessar a pagina do produto
 
         except Exception as e:
-            print("Something went wrong:", e)
+            print("Algo deu errado:", e)
             price = 'Error retrieving price'
 
-    # Captura genérica de preços se nenhum preço específico for encontrado
+    # Se ainda assim não achar um preço, dá um aviso genérico
     if not price:
         try:
             price = 'No generic price available'
         except Exception as e:
-            print("Error during generic price search:", e)
+            print("Erro durante a busca do preço:", e)
             price = 'Error retrieving price'
 
-    # Retorna o objeto JSON com as informações extraídas
+    # Retorna tudo que a gente conseguiu pegar
     return {
         'image': image['content'] if image and 'content' in image.attrs else image['src'] if image else 'No image available',
         'price': price if price else 'No price available',
         'title': title['content'] if title and 'content' in title.attrs else title.get_text(strip=True) if title else 'No title available',
-        'site': url.split('/')[2],
+        'site': url.split('/')[2],  
         'url': url
     }
 
 @app.route('/link-preview', methods=['POST'])
 def link_preview():
+    """ 
+    Rota que recebe um link e devolve uma prévia dele. 
+    Basicamente, é pra facilitar a vida e ter as informações na mão.
+    """
     data = request.json
-    url = data.get('url')
+    url = data.get('url')  # Pega a URL do JSON
     
     if not url:
-        return jsonify({'error': 'URL is required'}), 400
+        return jsonify({'error': 'URL is required'}), 400  # Se não passar a URL, não dá pra fazer nada
     
-    preview_data = scrape_page(url)
+    preview_data = scrape_page(url)  # Faz a chamada pra capturar os dados
     
     if not preview_data:
-        return jsonify({'error': 'Failed to fetch preview data'}), 500
-    
-    return jsonify(preview_data)
+        return jsonify({'error': 'Failed to fetch preview data'}), 500  # Se não conseguir pegar os dados, avisa
+
+    return jsonify(preview_data)  # Retorna os dados
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
