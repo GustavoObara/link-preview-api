cea6d4834b1214780cacc35fbd550ff1a2221a99
GustavoObara <luizobara@gmail.com>
2024-10-24 15:58:32 -0300

    alterações para iniciar o serviço no linux

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def create_app():
 # Executa a aplicação Flask com o modo de debug ativado
 if __name__ == '__main__':
     app = create_app()  
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
